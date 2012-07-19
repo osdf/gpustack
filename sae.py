66ea538765b526bd25334356e91e1cc149f6c4af
osdf <osendorf@gmail.com>
2012-07-19 12:22:50 +0200

    Compute sparsity averaged over time.

--- sae.py
+++ sae.py
@@ -3,6 +3,9 @@ A layer _pretrained_ as a Sparse Autoencoder.
 """
 
 
+import numpy as np
+
+
 from gnumpy import dot as gdot
 from gnumpy import zeros as gzeros
 from gnumpy import logistic
@@ -10,31 +13,37 @@ import gnumpy as gpu
 
 from tae import TAE
 from losses import bKL
-from layer import Layer
 from misc import Dsigmoid
 
 
 class SAE(TAE):
     def __init__(self, shape, rho=0.01, beta=3, params=None, **kwargs):
+        super(SAE, self).__init__(shape=shape, activ=logistic, params=params)
         self.rho = rho
         self.beta = beta
-        super(SAE, self).__init__(shape=shape, activ=logistic, params=params)
+        self.rho_hat = None
+        self.rho_hat_grad = None
 
     def __repr__(self):
         activ = str(self.activ).split()[1]
-        rep = "SAE-%s-%s-%s-%s"%(self.rho, self.beta, activ, self.shape)
+        score = str(self.score).split()[1]
+        rep = "SAE-%s-%s-%s-%s-%s"%(self.rho, self.beta, activ, score, self.shape)
         return rep
 
     def pt_score(self, params, inpts, **kwargs):
         hddn = logistic(gpu.dot(inpts, params[:self.m_end].reshape(self.shape)) + params[self.m_end:self.m_end+self.shape[1]])
         Z = gdot(hddn, params[:self.m_end].reshape(self.shape).T) + params[-self.shape[0]:]
 
-        rho_hat = hddn.mean(axis=0)
+        if self.rho_hat == None:
+            self.rho_hat = hddn.mean(axis=0)
+        else:
+            self.rho_hat *= 0.9
+            self.rho_hat += 0.1*hddn.mean(axis=0)
 
-        sparsity = self.beta * gpu.sum(bKL(self.rho, rho_hat))
+        sparsity = self.beta * gpu.sum(bKL(self.rho, self.rho_hat))
         sc = self.score(Z, inpts, addon=sparsity)
-        
-        return sc
+
+        return np.array([sc, sc-sparsity, sparsity, gpu.mean(self.rho_hat)])
 
     def pt_grad(self, params, inpts, **kwargs):
         g = gzeros(params.shape)
@@ -43,7 +52,14 @@ class SAE(TAE):
         hddn = logistic(gpu.dot(inpts, params[:self.m_end].reshape(self.shape)) + params[self.m_end:self.m_end+self.shape[1]])
         Z = gdot(hddn, params[:self.m_end].reshape(self.shape).T) + params[-self.shape[0]:]
 
-        rho_hat = hddn.mean(axis=0)
+        if self.rho_hat_grad == None:
+            self.rho_hat_grad = hddn.mean(axis=0)
+        else:
+            self.rho_hat_grad *= 0.9
+            self.rho_hat_grad += 0.1*hddn.mean(axis=0)
+
+#        rho_hat = hddn.mean(axis=0)
+        rho_hat = self.rho_hat_grad
         rho = self.rho
         sparsity = self.beta * gpu.sum(bKL(rho, rho_hat))
 
